4774e92092b17d7cc91ba8ce459694fc8e63160d
Zelda West <west@jhu.edu>
2025-03-25 11:18:30 -0400

    -Committing up state - have step checks for most conditions, save box.  Need to step away for a bit but can wrap up box when back as well as get unit tests udpated.

--- quizzes/simpli-ticnetoe/src/lib/gameboard.ts
+++ quizzes/simpli-ticnetoe/src/lib/gameboard.ts
@@ -8,17 +8,10 @@ import InvalidBoardError from '@src/types/errors/InvalidBoardError';
 const minVictorySize = 3, minBoardSize = 3;
 const maxVictorySize = 8, maxBoardSize = 10;
 
-enum stepType {
-  Vertical,
-  Horizontal,
-  Diagonal,
-  Box,
-}
-
-enum stepDirection {
-  Forwards,
-  Backwards,
-
+// Grid coordinates
+interface Coordinate {
+  x: number;
+  y: number;
 }
 
 /**
@@ -37,6 +30,10 @@ class Gameboard {
   // victorySize is how long a run must be to win.
   // square victories are expected to be floor(victorySize/2) to a side
   public readonly victorySize: number;
+
+  // victoryBoxSize is the winning condition where you create a 'box' within
+  // Due to potential to have odd victorySize values, this box may require more tokens overall than a linear victory
+  public readonly victoryBoxSize: number;
 
   // board defines the current gameboard state.  null indicates no player/empty space
   private board: (Player | null)[][];
@@ -63,6 +60,7 @@ class Gameboard {
 
     this.boardSize = size;
     this.victorySize = victorySize;
+    this.victoryBoxSize = Math.ceil(victorySize / 2);
     this.emptySpaces = this.boardSize ** 2;
     this.winner = null;
 
@@ -128,7 +126,7 @@ class Gameboard {
     this.emptySpaces--;
 
     // TODO Check for winner before returning
-    const winner = this.checkWinningCoordinate(this.heightMap[column] - 1, column);
+    const winner = this.checkWinningCoordinate(({x: this.heightMap[column] - 1, y: column} as Coordinate));
     if (winner !== null) {
       this.winner = winner;
     }
@@ -149,16 +147,12 @@ class Gameboard {
    * @param coords number[][] Board coordinates to compare.  Any number can be checked.
    * @return Player | null Player object if all coords match, or null if they do not
    */
-  private compareCoordinates(...coords: number[][]): Player | null {
+  private compareCoordinates(...coords: Coordinate[]): Player | null {
     let player;
     logger.debug('Check coords: ', coords);
 
     for (const v of coords) {
-      if (v.length !== 2) {
-        throw new Error('checkCoordinates error - did not get a valid x/y pair');
-      }
-
-      const boardValue = this.board[v[0]][v[1]];
+      const boardValue = this.board[v.x, v.y];
 
       // Return immediately if any are empty
       if (boardValue === null) {
@@ -188,60 +182,146 @@ class Gameboard {
    * Lots of ways you could try to optimize the hell out of the pathfinding, but given the size
    * of the board none really matter.
    *
-   * @param x x/row coordinate
-   * @param y y/col coordinate
+   * @param c Coordinate pair to check
    * @private
    * @return Player winning player, or null
    */
-  private checkWinningCoordinate(x: number, y: number): Player | null {
+  private checkWinningCoordinate(c: Coordinate): Player | null {
+    const p = this.board[c.x][c.y];
+    if (p === null) {
+      return null;
+    }
+
     // Corners win, no matter the victory size, but only if we're *in* a corner
-    if (x === (this.boardSize - 1 || 0) && y === (this.boardSize - 1 || 0)) {
+    if (c.x === (this.boardSize - 1 || 0) && c.y === (this.boardSize - 1 || 0)) {
       const checkCorners = this.compareCoordinates(
-        [0, 0],
-        [0, this.boardSize - 1],
-        [this.boardSize - 1, 0],
-        [this.boardSize - 1, this.boardSize - 1]);
+        {x: 0, y: 0},
+        {x: 0, y: this.boardSize - 1},
+        {x: this.boardSize - 1, y: 0},
+        {x: this.boardSize - 1, y: this.boardSize - 1},
+      );
       if (checkCorners instanceof Player) {
         return checkCorners;
       }
     }
 
-    // TODO Iterate through the conditions:
-    // Vertical - needs to have a neighbor above or below
-    // this.linearStep(,)
-    // Horizontal - needs to have a matching neighbor east or west
-    // Square - toughest to match, but needs to have at least one matching neighbor in a cardinal direction
-    // Diagonal - needs to have a matching neighbor in a non-cardinal direction
+    /**
+     * Iterate through the conditions
+     * Could reduce this down more # of lines wise but doesn't gain much to do so
+     * (eg by storing all the sets of steps into an array and iterating over it)
+     *
+     * If we wanted to add more unique win conditions then it'd be worthwhile
+     */
+
+    // Horizontal
+    const [, ECount] = this.walkBoard(p, c, {x: 1, y: 0}, true);
+    const [, WCount] = this.walkBoard(p, c, {x: -1, y: 0}, true);
+    if (ECount + WCount >= this.victorySize) {
+      this.winner = p;
+      return p;
+    }
+
+    // Vertical
+    const [, SCount] = this.walkBoard(p, c, {x: 0, y: 1}, true);
+    const [, NCount] = this.walkBoard(p, c, {x: 0, y: -1}, true);
+    if (SCount + NCount >= this.victorySize) {
+      this.winner = p;
+      return p;
+    }
+
+    // Diagonal - NE/SW
+    const [, NECount] = this.walkBoard(p, c, {x: 1, y: -1}, true);
+    const [, SWCount] = this.walkBoard(p, c, {x: -1, y: 1}, true);
+    if (NECount + SWCount >= this.victorySize) {
+      this.winner = p;
+      return p;
+    }
+
+    // Diagonal - NW/SE
+    const [, NWCount] = this.walkBoard(p, c, {x: -1, y: -1}, true);
+    const [, SECount] = this.walkBoard(p, c, {x: 1, y: 1}, true);
+    if (NWCount + SECount >= this.victorySize) {
+      this.winner = p;
+      return p;
+    }
+
+    /**
+     * TODO Look at the grouping around this coordinate
+     * TODO Refactor this out into
+     * We're looking for a box of size 'Math.ceil(victorySize / 2)'
+     * More complex than a line - 'this' coordinate could be in the middle, on the edge and matching
+     * neighbors could be dead ends rather than sides.
+     * So strategy - walk in the cardinal directions (which we've already done)
+     * If either axis is too short to win, we're done
+     * If both axis are long enough then we need to step along the axis'
+     * In theory, since this is Connect-4(ish), we'd never have anything 'above' us but doesn't make enough
+     * difference implementation wise to be worth addressing.
+     * If the total for both directions (plus our current space) is equal to or more than the winning stretch, game over
+     * If both are long enough then we need to start painting a picture of the grid
+     *
+     * Sequence:
+     * Step N/S , looking at E/W each time
+     * E/W must each step be greater than or equal to the previous row
+     *
+     */
+
+    // One of the cardinal directions is too short for a box
+    if (NCount + SCount < this.victoryBoxSize || ECount + WCount < this.victoryBoxSize) {
+      return null;
+    }
+
+    // Step vertically, but walk E/W each time
+    const boxRows = 1; // We know we have one already
+    if (NCount > 0) {
+
+    }
+    if (SCount > 0) {
+
+    }
+
+
 
     return null;
   }
 
   /**
-   * linearStep goes from the requested point along an x or y axis
+   * walkBoard goes from the requested point along an x/y axis, looking for matches
+   *
+   * @param p Player
+   * @param coords Coordinate
+   * @param step Coordinate Steps to use, as [x,y] - eg [1,0] means 'step once right'
+   * @param skip Skip 'this' coordinate and just step into the next
    * @private
    */
-  private linearStep(type: stepType /* lastStep, nextStep, stepDirection? */): Player | null {
+  private walkBoard(p: Player, coords: Coordinate, step: Coordinate, skip = false): [Player | null, number] {
+    const space = this.board[coords.x][coords.y];
 
-    // TODO
-    switch (type) {
-    case stepType.Vertical:
-      // TODO Iterate up/down
-      break;
-    case stepType.Horizontal:
-      // TODO Iterate left/right
-      break;
-    case stepType.Diagonal:
-      // TODO Iterate on a diagonal
-      break;
-
+    // If the space is empty, or doesn't match the player that came in, return null
+    if (space !== p) {
+      return [null, 0];
     }
 
-    return null;
+    // Make sure we didn't step outside the board
+    if (coords.x < 0 || coords.y < 0 || coords.x >= this.boardSize || coords.y >= this.boardSize) {
+      return [null, 0];
+    }
+
+    // Space matches, but we're at the edge of the board. Return
+    const nextSpace = {x: coords.x + step.x, y: coords.y + step.y} as Coordinate;
+    if (nextSpace.x < 0 || nextSpace.y < 0 || nextSpace.x >= this.boardSize || nextSpace.y >= this.boardSize) {
+      return [p, 1];
+    }
+
+    // Space matches, and we can keep going. Recurse
+    const [, tally] = this.walkBoard(p, nextSpace, step);
+    return [p, (skip) ? tally : tally + 1];
   }
 
   /**
    * boxStep will attempt to step over/down/back to try to discover a 'box' around
    * the originally requested coordinate
+   *
+   * Has a unique function as the logic is much different vs linear.
    *
    * @private
    */
